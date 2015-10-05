5d5890e022f66482f7f051d210aebb803135e934
Lucy Fang <lucyfang2@gmail.com>
2015-10-05 14:14:43 +0100

    Added example game

--- example_game.rb
+++ example_game.rb
@@ -29,16 +29,44 @@ fleet2.each do |ship|
   board2.place_ship_at_random(ship)
 end
 
-while game.winner == false
-  game.shoots_at_random
+counter = 0
+shot = ""
+while shot != "winner"
+  shot = game.shoots_at_random
+  counter += 1
 end
 
+# checking winner not using random shooting methods
 
+# game = Game.new
+#
+# player1 = Player.new
+# player2 = Player.new
+#
+# fleet1 = [Ship.new(2)]
+# fleet2 = [Ship.new(2)]
+#
+# board1 = Board.new(2)
+# board2 = Board.new(2)
+#
+# game.add_player(player1)
+# game.add_player(player2)
+#
+# player1.board = board1
+# player2.board = board2
+#
+# fleet1.each do |ship|
+#   board1.place_ship(ship, [1,1], 'east')
+# end
+#
+# fleet2.each do |ship|
+#   board2.place_ship(ship, [2,1], 'east')
+# end
+#
+# game.shoots([2,1])
 # game.shoots([1,1])
-# game.shoots([1,1])
-# game.shoots([10,10])
-# game.shoots([10,10])
-# 10.times{game.shoots_at_random}
+# winner = game.shoots([2,2])
+# p winner
 
 
 p board1.show_my_board
